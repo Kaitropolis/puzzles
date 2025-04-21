c7b83e4a8e125195208cb75cb92c179e310b27cf
Rio <riostockton@gmail.com>
2025-04-21 14:16:52 +0100

    Create maze with dfs

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,17 +1,146 @@
 class Maze {
   constructor() {
-    this.events();
+    this.container = document.getElementById("game-container");
+    this.rows = 15;
+    this.cols = 15;
+    this.graph = [];
+
+    this.createGraph();
+    this.createMaze();
   }
 
-  events() {
-    document
-      .getElementById("start-btn")
-      .addEventListener("click", this.createMaze.bind(this));
+  createGraph() {
+    let vertex = 0;
+
+    for (let row = 0; row < this.rows; ++row) {
+      const rowEl = document.createElement("tr");
+
+      for (let col = 0; col < this.cols; ++col) {
+        this.graph.push([]);
+        this.setVertexNeighbours(row, col, vertex);
+
+        const vertexEl = document.createElement("td");
+        vertexEl.id = vertex.toString();
+        vertexEl.className =
+          "text-center w-[30px] h-[30px] text-sm border-t-8 border-b-8 border-l-8 border-r-8 border-gray-300 text-white font-semibold";
+
+        //vertexEl.innerText = vertex;
+        rowEl.appendChild(vertexEl);
+        vertex++;
+      }
+
+      this.container.appendChild(rowEl);
+    }
   }
 
-  createMaze() {
-    console.log("start", this);
+  setVertexNeighbours(row, col, vertex) {
+    const neighbours = [];
+
+    const up = row > 0 ? vertex - this.cols : null;
+    const down = row < this.rows - 1 ? vertex + this.cols : null;
+    const left = col > 0 ? vertex - 1 : null;
+    const right = col < this.cols - 1 ? vertex + 1 : null;
+
+    if (up !== null) neighbours.push(up);
+    if (down !== null) neighbours.push(down);
+    if (left !== null) neighbours.push(left);
+    if (right !== null) neighbours.push(right);
+
+    this.graph[vertex] = neighbours;
   }
+
+  async createMaze() {
+    const path = this.dfs();
+    // Example Path
+    // [
+    //   [0, 3],
+    //   [3, 4],
+    //   [4, 5],
+    //   [5, 2],
+    //   [2, 1],
+    //   [5, 8],
+    //   [8, 7],
+    //   [7, 6],
+    // ];
+
+    for (let i = 0; i < path.length; ++i) {
+      await this.sleep(50);
+
+      const [from, to] = path[i];
+      const fromEl = document.getElementById(from.toString());
+      const toEl = document.getElementById(to.toString());
+
+      if (from === 0) {
+        //fromEl.classList.remove("border-l-8");
+        fromEl.classList.add("bg-green-400");
+      } else if (to === this.rows * this.cols - 1) {
+        //toEl.classList.remove("border-r-8");
+        toEl.classList.add("bg-red-400");
+      } else {
+        fromEl.classList.add("bg-blue-400");
+        toEl.classList.add("bg-blue-400");
+      }
+
+      if (from - this.cols === to) {
+        fromEl.classList.remove("border-t-8");
+        toEl.classList.remove("border-b-8");
+      } else if (from + this.cols === to) {
+        fromEl.classList.remove("border-b-8");
+        toEl.classList.remove("border-t-8");
+      } else if (from - 1 === to) {
+        fromEl.classList.remove("border-l-8");
+        toEl.classList.remove("border-r-8");
+      } else if (from + 1 === to) {
+        fromEl.classList.remove("border-r-8");
+        toEl.classList.remove("border-l-8");
+      }
+    }
+  }
+
+  // Use depth first search to create a path through the maze whilst visiting all vertices
+  dfs() {
+    const stack = [[null, 0]]; // Track vertices we are moving from and to
+    const visited = new Set();
+    const path = [];
+
+    while (stack.length > 0) {
+      const [fromVertex, toVertex] = stack.pop();
+
+      if (!visited.has(toVertex)) {
+        if (fromVertex !== null) path.push([fromVertex, toVertex]);
+        visited.add(toVertex);
+      }
+
+      const unvisitedNeighbours = this.shuffle(this.graph[toVertex]).filter(
+        (v) => !visited.has(v)
+      );
+
+      for (const neighbour of unvisitedNeighbours) {
+        stack.push([toVertex, neighbour]);
+      }
+    }
+
+    return path;
+  }
+
+  shuffle(array) {
+    const copy = [...array];
+    let currentIndex = copy.length;
+
+    while (currentIndex !== 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [copy[currentIndex], copy[randomIndex]] = [
+        copy[randomIndex],
+        copy[currentIndex],
+      ];
+    }
+
+    return copy;
+  }
+
+  sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 }
 
 new Maze();
--- scripts/towerOfHanoi.js
+++ scripts/towerOfHanoi.js
@@ -1,67 +1,67 @@
-let disks = 20
-const colours = ["green", "yellow", "red"]
-let isGameStarted = false
-let moves = 0
+let disks = 6;
+const colours = ["green", "yellow", "red"];
+let isGameStarted = false;
+let moves = 0;
 
 window.onload = () => {
-  insertDisks()
-  document.getElementById("start-btn").addEventListener("click", start)
-}
+  insertDisks();
+  document.getElementById("start-btn").addEventListener("click", start);
+};
 
 async function start() {
-  if (isGameStarted) return
+  if (isGameStarted) return;
 
-  isGameStarted = true
-  await hanoi(disks, 1, 3)
+  isGameStarted = true;
+  await hanoi(disks, 1, 3);
 }
 
 async function hanoi(n, from, to) {
-  const other = 6 - (from + to)
+  const other = 6 - (from + to);
 
   if (n === 1) {
-    await move(from, to)
-    return 1
+    await move(from, to);
+    return 1;
   }
 
-  const step1 = await hanoi(n - 1, from, other)
-  const step2 = await hanoi(1, from, to)
-  const step3 = await hanoi(n - 1, other, to)
+  const step1 = await hanoi(n - 1, from, other);
+  const step2 = await hanoi(1, from, to);
+  const step3 = await hanoi(n - 1, other, to);
 
-  return step1 + step2 + step3
+  return step1 + step2 + step3;
 }
 
 function insertDisks() {
-  const rod1 = document.getElementById("rod-1")
+  const rod1 = document.getElementById("rod-1");
 
-  let diskWidth = 30
-  let colourIndex = 0
+  let diskWidth = 30;
+  let colourIndex = 0;
 
   for (let disk = 1; disk <= disks; ++disk) {
-    colourIndex = colourIndex === 2 ? 0 : ++colourIndex
+    colourIndex = colourIndex === 2 ? 0 : ++colourIndex;
 
     rod1.insertAdjacentHTML(
       "beforeend",
       `<div class="w-[${diskWidth}px] h-[15px] bg-${colours[colourIndex]}-500 rounded"></div>`
-    )
+    );
 
-    diskWidth += 20
+    diskWidth += 20;
   }
 }
 
 async function move(from, to) {
-  await sleep(100)
+  await sleep(200);
 
-  const fromRod = document.getElementById(`rod-${from}`)
-  const toRod = document.getElementById(`rod-${to}`)
-  const disk = fromRod.firstElementChild
+  const fromRod = document.getElementById(`rod-${from}`);
+  const toRod = document.getElementById(`rod-${to}`);
+  const disk = fromRod.firstElementChild;
 
-  if (!disk) return
+  if (!disk) return;
 
-  ++moves
-  document.getElementById("moves").innerHTML = `Moves: ${moves}`
+  ++moves;
+  document.getElementById("moves").innerHTML = `Moves: ${moves}`;
 
-  fromRod.removeChild(disk)
-  toRod.prepend(disk)
+  fromRod.removeChild(disk);
+  toRod.prepend(disk);
 }
 
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms))
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
